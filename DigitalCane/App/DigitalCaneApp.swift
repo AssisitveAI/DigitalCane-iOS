7aa6e197442abbf41553733a75bf8fc7f3e902e1
Assistive AI Lab <hyun.ka@kaist.ac.kr>
2025-12-31 13:52:40 +0900

    Fix Invalid redeclaration of SplashView error

--- DigitalCane/App/DigitalCaneApp.swift
+++ DigitalCane/App/DigitalCaneApp.swift
@@ -86,34 +86,4 @@ struct DigitalCaneApp: App {
     }
 }
 
-// 스플래시 뷰 정의 (파일 인식 문제 해결을 위해 여기에 포함)
-struct SplashView: View {
-    @State private var isAnimating = false
-    
-    var body: some View {
-        ZStack {
-            Color.black.ignoresSafeArea()
-            
-            VStack(spacing: 20) {
-                Image(systemName: "figure.walk")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 100, height: 100)
-                    .foregroundColor(.yellow)
-                    .scaleEffect(isAnimating ? 1.1 : 1.0)
-                    .animation(Animation.easeInOut(duration: 1.0).repeatForever(autoreverses: true), value: isAnimating)
-                
-                Text("Digital Cane")
-                    .font(.system(size: 40, weight: .bold))
-                    .foregroundColor(.white)
-                
-                Text("당신의 눈이 되어드릴게요")
-                    .font(.system(size: 18))
-                    .foregroundColor(.gray)
-            }
-        }
-        .onAppear {
-            isAnimating = true
-        }
-    }
-}
+
